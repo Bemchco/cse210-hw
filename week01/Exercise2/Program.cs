9193f6e7cd80e18c0de2e0a7b4a13b186c05a81e
Bemchco <bchinchillacorleto@byupathway.edu>
2025-07-05 01:39:22 -0600

    Finally :D

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage?");
+        string letter = "";
+        string gradeVal = Console.ReadLine();
+        int grade = int.Parse(gradeVal);
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"{letter}");
+
+
+        if (letter == "A" || letter == "B" || letter == "C")
+        {
+            Console.WriteLine("Congratualions you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying you can do it!");
+        }
+
+
+
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,51 @@
 using System;
-
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.Intrinsics.Arm;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+
+        List<int> numbers = new List<int>();
+        string value;
+        Console.Write("Enter a list of numbers, type 0 when finished.");
+        int number = 0;
+        double sum = 0;
+
+
+        do
+        {
+            Console.Write("Enter Number:");
+            value = Console.ReadLine();
+            number = int.Parse(value);
+            if (number > 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);
+
+        int largest = numbers[0];
+        foreach (int numeric in numbers)
+        {
+            sum += numeric;
+            if (numeric > largest)
+            {
+                largest = numeric;
+            }
+        }
+
+        double counter = numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = sum / counter;
+        Console.WriteLine($"The average is {average}");
+
+        Console.WriteLine($"The largest number is: {largest}");
+
+
     }
 }
