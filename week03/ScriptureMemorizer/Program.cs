fa0f3633b7cc5b06ded850093d137a9754ee0ee6
Bemchco <bchinchillacorleto@byupathway.edu>
2025-07-18 00:44:02 -0600

    Coments on little upgrade

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Runtime.InteropServices;
 
+// Scripture Memorizer
+// This program is a simple scripture memorization tool that allows users to hide random words from a given scripture text.
+// Users can repeatedly hide words and even though they are random words, the same words will not be hidden and thus every time they hit enter a new set of words will hide until all words are hidden or they choose to exit.
 class Program
 {
     static void Main(string[] args)
